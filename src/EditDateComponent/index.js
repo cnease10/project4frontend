b478cd9096fdeebd851cedb36a297437d320137c
cnease10 <cierra.nease10@gmail.com>
2019-12-05 12:35:32 -0700

    have an edit form in a modal

--- src/EditDateComponent/index.js
+++ src/EditDateComponent/index.js
@@ -1,1 +1,22 @@
 //need a form/modal that will update a date idea from a user
+import React from 'react';
+import {Form, Button, Header, Modal } from 'semantic-ui-react';
+
+const EditDog = (props) => {
+        return (
+            <Modal open={props.openModal}>
+                <h1>Edit A Date!</h1>
+                <Modal.Content>
+                <Form onSubmit={props.handleEdit}>
+                    <Header as="h2">Name:</Header>
+                    <Form.Input focus type="text" name='name' value={props.dateEdit.name} onChange={props.handleEdit}/>
+                    <Header as="h2" >Description:</Header>
+                    <Form.Input focus type="text" name='description' value={props.dateEdit.description} onChange={props.handleEdit}/>
+                    <Button type="submit">Edit Date</Button>
+                </Form>
+                </Modal.Content>
+            </Modal>
+        )
+}
+
+export default EditDog
--- src/MainComponent/index.js
+++ src/MainComponent/index.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react'
 import HomeComponent from '../HomeComponent ';
 import CreateDateComponent from '../CreateDateComponent'
+import EditDateComponent from '../EditDateComponent'
+
+
 class MainComponent extends Component {
     constructor() {
         super();
 
         this.state = {
-            dates: []
+            dates: [],
+            userdates: [],
+            dateEdit: {
+                name: '',
+                description: ''
+            }
         }
         
     }
@@ -44,7 +52,7 @@ class MainComponent extends Component {
             const parsedResponse = await dateResponse.json();
             if (parsedResponse.status.code === 200) {
                 this.setState({
-                    dates: [...this.state.dates, parsedResponse.data],
+                    userdates: [...this.state.userdates, parsedResponse.data],
                     
                 })
                 console.log('hitting add date')
@@ -56,11 +64,60 @@ class MainComponent extends Component {
             console.log(err)
         }
     }
+    //EDIT DATE
+    handleEdit = (e) => {
+        this.setState({
+            dateEdit: {
+                ...this.state.dateEdit, [e.currentTarget.name]: e.currentTarget.value
+            }
+        })
+    }
+    //OPEN MODAL FOR EDIT
+    openModal = (date) => {
+        console.log(date)
+        this.setState({
+            // showModal: true,
+            dateEdit: {
+                ...date
+            }
+        })
+    }
+    // CLOSE MODAL FOR EDIT
+    close = async(e) => {
+        e.preventDefault();
+
+        try{
+            const editResponse = await fetch(process.env.REACT_APP_API_URL + '/api/v1/dates/' + this.state.dateEdit.id, {
+                method: 'PUT',
+                body: JSON.stringify(this.state.dateEdit),
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const editResponseParsed = await editResponse.json();
+            const newDateArray = this.state.userdates.map((date) => {
+                if (date.id === editResponseParsed.data.id) {
+                    date = editResponseParsed.data
+                }
+                return date;
+            })
+            this.setState({
+                userdates: newDateArray,
+                // showModal: false,
+            })
+            this.setState({ state: this.state });
+        } catch(err) {
+            console.log(err)
+        }
+    }
+    
     render() {
         return(
             <div>
-                <CreateDateComponent dates={this.state.dates} addDate={this.addDate}/>
-                <HomeComponent getDates={this.getDates} dates={this.state.dates}/>
+                {/* <EditDateComponent handleEdit={this.handleEdit} openModal={this.openModal} closeModal={this.close} dateEdit={this.state.dateEdit} /> */}
+                <CreateDateComponent userdates={this.state.userdates} addDate={this.addDate}/>
+                <HomeComponent getDates={this.getDates} dates={this.state.userdates}/>
             </div>
         )
     }
